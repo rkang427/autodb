5aa572549d236ab1ea3b9354d98c107f547fe19c
serhatprogramming <serhatprogramming@gmail.com>
2024-10-06 11:46:46 -0400

    vehicle and vehiclecolors table added.

--- Phase_2/team006_p2_schema.sql
+++ Phase_2/team006_p2_schema.sql
@@ -31,3 +31,33 @@ CREATE TABLE vendor (
     phone_number VARCHAR(12) NOT NULL,
     state VARCHAR(120) NOT NULL
 );
+
+-- Vehicle table
+CREATE TABLE vehicle (
+    VIN VARCHAR(17) PRIMARY KEY,
+    sale_date DATE NULL,
+    sale_price DECIMAL(19,4) NULL,
+    total_parts_price DECIMAL(19,4) NULL,
+    description VARCHAR(150) NULL,
+    horsepower SMALLINT NOT NULL,
+    year YEAR NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    purchase_price DECIMAL(19,4) NOT NULL,
+    purchase_date DATE NOT NULL,
+    condition VARCHAR(10) NOT NULL,
+    CHECK (condition IN ('Excellent', 'Very Good', 'Good', 'Fair')),
+    fuel_type VARCHAR(10) NOT NULL,
+    CHECK (fuel_type IN ('Gas', 'Diesel', 'Natural Gas', 'Hybrid', 'Plugin Hybrid', 'Battery',  'Fuel Cell')),
+    buyer_username VARCHAR(50) NOT NULL,
+    FOREIGN KEY (buyer_username) REFERENCES VehicleBuyer(username),
+    seller_username VARCHAR(50) NULL,
+    FOREIGN KEY (seller_username) REFERENCES VehicleSeller(username)
+);
+
+-- VehicleColors
+CREATE TABLE VehicleColors (
+    VIN VARCHAR(17) NOT NULL,
+    color VARCHAR(10) NOT NULL,
+    CHECK (color IN ('Aluminum', 'Beige','Black','Blue','Brown','Bronze','Claret','Copper','Cream','Gold','Gray','Green','Maroon','Metallic','Navy','Orange','Pink','Purple','Red','Rose','Rust','Silver','Tan','Turquoise','White','Yellow')),
+    FOREIGN KEY (VIN) REFERENCES Vehicle(VIN)
+)

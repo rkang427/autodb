933901aecd7be8766a97a482acca368cee9b78d5
kdelee3 <kdelee3@gatech.edu>
2024-10-30 13:28:24 -0400

    add search API and initial tests
    this is missing lots of testing and additionally the logic around
    setting "include_parts_not_ready" to either false or true depending on
    the user type. Also would need to validate if the user_type was allowed
    to set filter to something other than "unsold".

--- Phase_3/backend/routes/vehicle.js
+++ Phase_3/backend/routes/vehicle.js
@@ -1,14 +1,128 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
 const pool = require('../config/db');
-const { vehicleGetValidator, vehiclePostValidator } = require('./validators');
-const PG_ERROR_CODES = require('../config/constants');
+const {
+  vehicleGetValidator,
+  vehiclePostValidator,
+  vehicleSearchValidator,
+} = require('./validators');
+const { PG_ERROR_CODES } = require('../config/constants');
 
 const router = express.Router();
 
 console.log('loading vehicle routes');
 // GET endpoint to check if vehicle exists by vin
-router.get('/', vehicleGetValidator, async (req, res) => {
+router.get('/search', vehicleGetValidator, async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const {
+      vin,
+      color,
+      manufacturer,
+      vehicle_type,
+      fuel_type,
+      model_year,
+      keyword,
+      filter_type,
+    } = req.query;
+    // TODO: SET include_parts_not_ready depending on user_type
+    // TODO: check if filter_type is value allowed for user_type
+    const include_parts_not_ready = false;
+
+    const query = `
+      SELECT
+        vw.vin,
+        vw.vehicle_type,
+        vw.manufacturer,
+        vw.model,
+        vw.model_year,
+        vw.fuel_type,
+        vw.colors,
+        vw.horsepower,
+        vw.sale_price
+      FROM (
+        SELECT
+          v.vin,
+          v.vehicle_type,
+          v.manufacturer,
+          v.model,
+          v.description,
+          v.model_year,
+          v.fuel_type,
+          v.horsepower,
+          v.purchase_price,
+          v.sale_date,
+          STRING_AGG(vc.color, ', ') AS colors,
+          ROUND((1.25 * v.purchase_price) + (1.1 * v.total_parts_price), 2) AS sale_price
+        FROM vehicle AS v
+        LEFT JOIN vehicle_color AS vc ON v.vin = vc.vin
+        GROUP BY
+          v.vin,
+          v.vehicle_type,
+          v.manufacturer,
+          v.model,
+          v.model_year,
+          v.fuel_type,
+          v.horsepower,
+          v.purchase_price,
+          v.sale_date
+      ) AS vw
+      WHERE
+        (vw.vin NOT IN (
+          SELECT po.vin
+          FROM parts_order AS po
+          INNER JOIN part AS p ON po.parts_order_number = p.parts_order_number
+          WHERE p.status <> 'installed') OR $1)
+        AND ((vw.sale_date IS NULL AND $2 = 'unsold')
+        OR (vw.sale_date IS NOT NULL AND $2 = 'sold') OR ($2 = 'both'))
+        AND (vw.vehicle_type = $3 OR $3 IS NULL)
+        AND (vw.manufacturer = $4 OR $4 IS NULL)
+        AND (vw.model_year = $5 OR $5 IS NULL)
+        AND (vw.fuel_type = $6 OR $6 IS NULL)
+        AND (vw.colors LIKE $7 OR $7 IS NULL)
+        AND (LOWER(vw.vin) = LOWER($8) OR $8 IS NULL)
+        AND (
+          (vw.manufacturer ILIKE $9 OR $9 = '')
+          OR (vw.model ILIKE $9 OR $9 = '')
+          OR (vw.model_year::TEXT ILIKE $9 OR $9 = '')
+          OR (vw.description ILIKE $9 OR $9 = '')
+        )
+      ORDER BY vw.vin ASC;
+    `;
+
+    const values = [
+      include_parts_not_ready, // $1
+      filter_type || 'unsold', // $2
+      vehicle_type || null, // $3
+      manufacturer || null, // $4
+      model_year || null, // $5
+      fuel_type || null, // $6
+      color ? `%${color}%` : null, // $7
+      vin || null, // $8
+      keyword ? `%${keyword}%` : '', // $9
+    ];
+
+    const result = await pool.query(query, values);
+
+    if (result.rows.length === 0) {
+      return res
+        .status(404)
+        .send('No vehicle found with the provided criteria');
+    }
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error('Database connection error:', error);
+    res.status(500).send('Error connecting to the database');
+  }
+});
+
+router.get('/', vehicleSearchValidator, async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {

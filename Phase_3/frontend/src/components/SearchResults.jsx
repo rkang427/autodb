45d18cb2ba43d661cab8c422615d6fe9b83fab86
serhatprogramming <serhatprogramming@gmail.com>
2024-11-06 14:46:29 -0500

    eslint disable removed.

--- Phase_3/frontend/src/components/SearchResults.jsx
+++ Phase_3/frontend/src/components/SearchResults.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 const SearchResults = ({ searchResults, loggedInUser }) => {
   if (searchResults.length == 0) return null;
 
